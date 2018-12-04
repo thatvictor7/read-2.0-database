c3d94cc73428df28b758c530f75f805a4340d945
thatvictor7 <montoya.victor7@gmail.com>
2018-12-04 15:32:09 -0700

    adding data and knex seeds

--- knexfile.js
+++ knexfile.js
@@ -3,42 +3,13 @@
 module.exports = {
 
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
+    client: 'pg',
+    connection: 'postgres://localhost/gbooks'
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
+  
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    connection: process.env.DATABASE_URL + '?ssl=true'
   }
 
 };
